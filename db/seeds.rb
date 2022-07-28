93f4cfe3c254c1800d3ec3a27def7a86f5306004
markymat <javascript7now>
2022-07-29 01:20:38 +0200

    Add devise gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding started.."
+ 1.upto(5) do |i|
+    Employee.create(email: "employee#{i}@altavista.com", password: "password")
+ end
+puts "...seeding done"
